69dcf5a50dcd99562e1223f1d91d471b6371d404
Navaneethan15 <navaneethanmani414@gmail.com>
2023-09-07 11:25:02 +0530

    toggle

--- src/components/Navbar/Navabar.jsx
+++ src/components/Navbar/Navabar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Navabar.css';
+import Toggle from '../Toggle/Toggle';
 
 const Navabar =() =>{
     return(
---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Toggle.css'
+import { BsFillSunFill } from 'react-icons/bs';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+
+const Toggle = () => {
+  return (
+    <div className="toggle">
+            <BsFillMoonStarsFill/>
+            <BsFillSunFill/>
+            <div className="t-button">
+                
+            </div>
+    </div>
+  )
+}
+
+export default Toggle
